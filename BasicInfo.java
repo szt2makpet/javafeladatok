759bec3002f07b12b068136cd09d3dcd13a1dc12
szt2makpet <szt2makpet@vasvari.org>
2023-09-19 07:10:49 +0200

    feladat 16

---
+++ BasicInfo.java
@@ -0,0 +1,15 @@
+public class BasicInfo {
+    public static void main(String[] args) {
+        // Define variables
+        String name = "John Doe";
+        int age = 30;
+        double heightInMeters = 1.75;
+        boolean isMarried = false;
+
+        // Print the values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + heightInMeters + " meters");
+        System.out.println("Married: " + isMarried);
+    }
+}
